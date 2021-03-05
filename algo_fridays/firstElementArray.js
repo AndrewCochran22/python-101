3b2dde3b2433696297ec8924ebf2101122bbecc1
Andrew Cochran <acoch22@gmail.com>
2021-03-05 14:02:56 -0500

    added js exercises

---
+++ algo_fridays/firstElementArray.js
@@ -0,0 +1,7 @@
+const array = [1, 2, 3, 4, 5]
+
+const n = 3
+
+const newArray = array.slice(0, n)
+
+console.log(newArray)
---
+++ algo_fridays/isArray.js
@@ -0,0 +1,7 @@
+console.log(Array.isArray(1))
+
+console.log(Array.isArray([1, 2, 3, 4, 5]))
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(Array.isArray(numbers))
